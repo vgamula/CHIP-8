b291c63d5543eeec1909ced3a0fa66ad8505a7da
Volodymyr Gamula <v.gamula@gmail.com>
2019-12-28 00:41:33 +0200

    Something is drawing

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 
-use rand::random;
+use rand;
 
 const MEMORY_SIZE: usize = 4096;
 const REGISTERS_COUNT: usize = 16;
@@ -102,11 +102,44 @@ impl Chip8 {
         self.canvas.present();
     }
 
-    pub fn get_x(&self) -> usize {
+    fn update_screen(&mut self) {
+        if !self.draw_flag {
+            return;
+        }
+
+        let scale: i32 = 10;
+        for y in 0..32 {
+            for x in 0..64 {
+                let pos = y * 32 + x;
+                let pixel = if self.gfx[pos] == 1 { 255 } else { 0 };
+                self.canvas.set_draw_color(Color::RGB(pixel, pixel, pixel));
+                // refactor this mess
+                let rect = Rect::new(x as i32 * scale, y as i32 * scale, scale as u32, scale as u32);
+                if pixel == 255 {
+                    println!("{:?}", rect);
+                }
+                self.canvas.fill_rect(rect).unwrap();
+            }
+        }
+
+        self.canvas.present();
+
+        self.draw_flag = false;
+    }
+
+    fn get_x(&self) -> usize {
         ((self.opcode & 0x0F00) >> 8) as usize
     }
 
-    pub fn get_kk(&self) -> u8 {
+    fn get_y(&self) -> usize {
+        ((self.opcode & 0x00F0) >> 4) as usize
+    }
+
+    fn get_n(&self) -> usize {
+        ((self.opcode & 0x00F0) >> 4) as usize
+    }
+
+    fn get_kk(&self) -> u8 {
         ((self.opcode & 0x00FF) as u8)
     }
 
@@ -119,38 +152,71 @@ impl Chip8 {
             0xC000 => self.handle_CXXX(),
             0xD000 => self.handle_DXXX(),
             _ => {
-                panic!("Unhandled opcode {:x}", self.opcode);
+                // continue;
+                // // self.pc += 2;
+                // println!("{:x}", self.opcode);
+                // panic!("Unhandled opcode {:x}", self.opcode);
             }
         }
+
+        if self.delay_timer > 0 {
+            self.delay_timer -= 1;
+        }
+
+        if self.sound_timer > 0 {
+            self.sound_timer -= 1;
+        }
+
+        self.update_screen();
     }
 
-    pub fn handle_3XXX(&mut self) {
+    fn handle_3XXX(&mut self) {
         if self.V[self.get_x()] == self.get_kk() {
             self.pc += 2;
         }
         self.pc += 2;
     }
 
-    pub fn handle_AXXX(&mut self) {
+    fn handle_AXXX(&mut self) {
         self.I = self.opcode & 0x0FFF;
         self.pc += 2;
     }
 
-    pub fn handle_CXXX(&mut self) {
+    fn handle_CXXX(&mut self) {
         let randomized_value = rand::random::<u8>();
         self.V[self.get_x()] = randomized_value & self.get_kk();
         self.pc += 2;
     }
 
-    pub fn handle_DXXX(&mut self) {
+    fn handle_DXXX(&mut self) {
+        // slightly rewritten handling 0xDXYN from
+        // http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
+        let x = self.get_x();
+        let y = self.get_y();
+        let height = self.get_n();
+        let mut pixel: u8;
 
+        self.V[0xF] = 0;
+
+        for yline in 0..height {
+            let pixel = self.memory[(self.I as usize)+ yline];
+            for xline in 0..8 {
+                if (pixel & (0x80 >> xline)) != 0 {
+                    let pos = x + xline + (y + yline) * 64;
+                    if self.gfx[pos] == 1 {
+                        self.V[0xF] = 1;
+                    }
+                    self.gfx[pos] ^= 1;
+                }
+            }
+        }
+        
+        self.draw_flag = true;
         self.pc += 2;
     }
 }
 
 fn main() {
-
-
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
     let _window = video_subsystem
@@ -165,7 +231,8 @@ fn main() {
 
     let mut cpu = Chip8::new(canvas);
     cpu.load_game("disks/MAZE".to_string());
-    cpu.test_draw();
+    // cpu.load_game("disks/PICTURE".to_string());
+    // cpu.test_draw();
 
     println!("Lift off!");
 
